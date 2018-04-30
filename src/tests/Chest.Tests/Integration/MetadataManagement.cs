8b67f8db66ebf192458505511d21f77d73240c9d
waqas zamir <waqas.zamir@lykke.com>
2018-04-30 16:00:16 +0500

    MTC-25 simplified tests

--- src/tests/Chest.Tests/Integration/MetadataManagement.cs
+++ src/tests/Chest.Tests/Integration/MetadataManagement.cs
@@ -5,6 +5,8 @@ namespace Chest.Tests.Integration
 {
     using System.Collections.Generic;
     using System.Globalization;
+    using System.Net;
+    using System.Threading.Tasks;
     using Chest.Client;
     using Chest.Dto;
     using Chest.Tests.Sdk;
@@ -20,7 +22,84 @@ namespace Chest.Tests.Integration
         }
 
         [Scenario]
-        public void CanGetAndAddMetdata()
+        public void GetShouldReturnNotFound()
+        {
+            // arrange
+            var client = new MetadataClient(this.ServiceUrl);
+            var key = "Unknown";
+
+            HttpException exception = null;
+
+            $"Given a key: {key} that was never added to the metadata service before"
+                .x(() =>
+                {
+                });
+
+            $"When I query the metadata for that key: {key}"
+                .x(async () =>
+                {
+                    // arrange
+                    try
+                    {
+                        await client.GetMetadata(key).ConfigureAwait(false);
+                    }
+                    catch (HttpException exp)
+                    {
+                        exception = exp;
+                    }
+                });
+
+            $"Then I get a HTTP 404 Not Found response"
+                .x(() =>
+                {
+                    Assert.NotNull(exception);
+                    Assert.Equal(HttpStatusCode.NotFound, exception.StatusCode);
+                    Assert.NotNull(exception.ContentMessage);
+                });
+        }
+
+        [Scenario]
+        public void CanAddMetadata()
+        {
+            var client = new MetadataClient(this.ServiceUrl);
+            var key = "456987";
+
+            var expected = new Metadata
+            {
+                Key = key,
+                Data = new Dictionary<string, string>
+                {
+                    { "account_number", key },
+                    { "margin_account", "MA01" },
+                    { "reference_account", "RF11" },
+                    { "bank_identification_reference", "BIR11" },
+                }
+            };
+
+            Metadata actual = null;
+
+            $"Given the metadata for key: {key}"
+                .x(async () =>
+                {
+                    await client.Add(expected).ConfigureAwait(false);
+                });
+
+            $"When try to get metadata for the key: {key}"
+                .x(async () =>
+                {
+                    actual = await client.GetMetadata(key).ConfigureAwait(false);
+                });
+
+            "Then the fetched metadata should be same"
+                .x(() =>
+                {
+                    Assert.NotNull(actual);
+                    actual.Should().BeEquivalentTo(expected);
+                });
+        }
+
+        [Scenario]
+        public void ShouldNotAddKeyMultipleTimes()
         {
             var client = new MetadataClient(this.ServiceUrl);
             var key = "Some-Unique-Key";
@@ -32,50 +111,20 @@ namespace Chest.Tests.Integration
                 {
                     { "account_number", key },
                     { "margin_account", "MA02" },
-                    { "reference_account", "RF11" },
-                    { "bank_identification_reference", "BIR11" },
+                    { "reference_account", "RF12" },
+                    { "bank_identification_reference", "BIR12" },
                 }
             };
 
-            "Given the metadata for key doesn't exist, When try to get metadata for the key, Then system should should return 404 NotFound"
-                .x(async () =>
-                {
-                    try
-                    {
-                        var response = await client.GetMetadata(key).ConfigureAwait(false);
-                        Assert.Null(response);
-                    }
-                    catch (HttpException exp)
-                    {
-                        if (exp.StatusCode == System.Net.HttpStatusCode.NotFound)
-                        {
-                            Assert.NotNull(exp.ContentMessage);
-                            return;
-                        }
-                        throw;
-                    }
-                    catch
-                    {
-                        throw;
-                    }
-                });
+            HttpException httpException = null;
 
-            "Given the metadata for the key doesn't exist, When try to add metadata for the key, Then system should add data"
+            $"Given the metadata for key: {key}"
                 .x(async () =>
                 {
                     await client.Add(expected).ConfigureAwait(false);
                 });
 
-            "Given the metadata for the key exist, When try to get metadata for the key, Then system should return the same metadata"
-                .x(async () =>
-                {
-                    var response = await client.GetMetadata(key).ConfigureAwait(false);
-                    Assert.NotNull(response);
-
-                    response.Should().BeEquivalentTo(expected);
-                });
-
-            "Given the metadata for key already exist, When try to add metadata for the key again, Then system should return 409 Conflict"
+            $"When try to add metadata again for key: {key}"
                 .x(async () =>
                 {
                     try
@@ -84,39 +133,68 @@ namespace Chest.Tests.Integration
                     }
                     catch (HttpException exp)
                     {
-                        if (exp.StatusCode == System.Net.HttpStatusCode.Conflict)
-                        {
-                            Assert.NotNull(exp.ContentMessage);
-
-                            return;
-                        }
-
-                        throw;
+                        httpException = exp;
                     }
                 });
 
-            "Given the metadata for key already exist, When try to add metadata for the key but in different casing, Then system should return 409 Conflict"
+            $"Then system should return 409 Conflict"
+                .x(() =>
+                {
+                    Assert.Equal(HttpStatusCode.Conflict, httpException.StatusCode);
+                    Assert.NotNull(httpException.ContentMessage);
+                });
+        }
+
+        [Scenario]
+        public void ShouldNotAddKeyMultipleTimesEvenInDifferentCase()
+        {
+            var client = new MetadataClient(this.ServiceUrl);
+            var key = "Case-Sensitive-Key";
+
+            var expected = new Metadata
+            {
+                Key = key,
+                Data = new Dictionary<string, string>
+                {
+                    { "account_number", key },
+                    { "margin_account", "MA03" },
+                    { "reference_account", "RF13" },
+                    { "bank_identification_reference", "BIR13" },
+                }
+            };
+
+            HttpException httpException = null;
+
+            $"Given the metadata for key: {key}"
                 .x(async () =>
                 {
-                    try
-                    {
+                    await client.Add(expected).ConfigureAwait(false);
+                });
+
 #pragma warning disable CA1308 // Normalize strings to uppercase
-                        expected.Key = key.ToLower(CultureInfo.InvariantCulture);
+            key = key.ToLower(CultureInfo.InvariantCulture);
 #pragma warning restore CA1308 // Normalize strings to uppercase
 
+            $"When try to add metadata again for same key: {key} but in different case"
+                .x(async () =>
+                {
+                    try
+                    {
+                        expected.Key = key;
+
                         await client.Add(expected).ConfigureAwait(false);
                     }
                     catch (HttpException exp)
                     {
-                        if (exp.StatusCode == System.Net.HttpStatusCode.Conflict)
-                        {
-                            Assert.NotNull(exp.ContentMessage);
-
-                            return;
-                        }
-
-                        throw;
+                        httpException = exp;
                     }
+                });
+
+            $"Then system should return 409 Conflict"
+                .x(() =>
+                {
+                    Assert.Equal(HttpStatusCode.Conflict, httpException.StatusCode);
+                    Assert.NotNull(httpException.ContentMessage);
                 });
         }
     }
