cbbd0cd5a561c42a000def898cb381f23d26434f
waqas zamir <waqas.zamir@lykke.com>
2018-06-13 12:55:47 +0500

    TUA-344 added more columns and migrations

--- src/Chest/Data/Migrations/20180613072806_ModelExtension.Designer.cs
+++ src/Chest/Data/Migrations/20180613072806_ModelExtension.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace Chest.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180613072806_ModelExtension")]
+    partial class ModelExtension
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
