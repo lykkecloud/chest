c2bf450a7ff96e2774c2ce77060f2cdf9d17bef6
waqas zamir <waqas.zamir@lykke.com>
2018-12-07 19:45:50 +0500

    LT-620 fixed integration tests to run on SQL server database - removed postgres from ChestFixture - used connection string from testsettings.json - writen steps in readme to run integration test

--- src/tests/Chest.Tests/Sdk/ChestFixture.cs
+++ src/tests/Chest.Tests/Sdk/ChestFixture.cs
@@ -8,7 +8,6 @@ namespace Chest.Tests.Sdk
     using System.Globalization;
     using System.IO;
     using System.Net.Http;
-    using System.Net.Sockets;
     using System.Threading;
     using Chest.Client;
     using Chest.Client.AutorestClient;
@@ -16,29 +15,23 @@ namespace Chest.Tests.Sdk
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
     using Newtonsoft.Json.Serialization;
-    using Npgsql;
 
     public sealed class ChestFixture : IDisposable
     {
-        private readonly string ConnectionString;
-
-        private readonly string PostgresPassword;
+        private readonly string connectionString = "<pass_it_from_testsettings.json>";
 
         private static readonly string DockerContainerId = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture).Substring(12);
 
-        private readonly Process postgresProcess;
         private readonly Process chestProcess;
 
         public ChestFixture()
         {
             var config = new ConfigurationBuilder().AddJsonFile("testsettings.json").Build();
 
+            this.connectionString = config.GetConnectionString("Chest");
+
             this.ServiceUrl = new Uri(config.GetValue<string>("serviceUrl"));
 
-            PostgresPassword = config.GetValue<string>("PostgresPassword");
-            ConnectionString = $"Host=localhost;Database=Chest;Username=postgres;Password={PostgresPassword};";
-
-            this.postgresProcess = this.StartPostgres();
             this.chestProcess = this.StartChest();
         }
 
@@ -55,16 +48,6 @@ namespace Chest.Tests.Sdk
             }
 
             this.chestProcess.Dispose();
-
-            try
-            {
-                this.postgresProcess.Kill();
-            }
-            catch (InvalidOperationException)
-            {
-            }
-
-            this.postgresProcess.Dispose();
 
             // NOTE (Cameron): Remove the docker container.
             Process.Start(new ProcessStartInfo("docker", $"stop {DockerContainerId}"))
@@ -84,41 +67,6 @@ namespace Chest.Tests.Sdk
             return settings;
         }
 
-        private Process StartPostgres()
-        {
-            var process = Process.Start(
-                new ProcessStartInfo("docker", $"run --rm --name {DockerContainerId} -e POSTGRES_PASSWORD={PostgresPassword} -e POSTGRES_DB=Chest -p 5432:5432 postgres:10.1-alpine")
-                {
-                    UseShellExecute = true,
-                });
-
-            // NOTE (Cameron): Trying to find a sensible value here so as to not throw during a debug session.
-            Thread.Sleep(3500);
-
-            using (var connection = new NpgsqlConnection(ConnectionString))
-            {
-                var attempt = 0;
-                while (true)
-                {
-                    Thread.Sleep(500);
-                    try
-                    {
-                        connection.Open();
-                        break;
-                    }
-                    catch (Exception ex) when (ex is NpgsqlException || ex is SocketException || ex is EndOfStreamException)
-                    {
-                        if (++attempt >= 20)
-                        {
-                            throw;
-                        }
-                    }
-                }
-            }
-
-            return process;
-        }
-
         [DebuggerStepThrough]
         private Process StartChest()
         {
@@ -128,7 +76,7 @@ namespace Chest.Tests.Sdk
                 Path.DirectorySeparatorChar);
 
             Process.Start(
-                new ProcessStartInfo("dotnet", $"run -p {path} --connectionString \"{ConnectionString}\"")
+                new ProcessStartInfo("dotnet", $"run -p {path} --connectionString \"{connectionString}\"")
                 {
                     UseShellExecute = true,
                 });
