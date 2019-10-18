79ba5680f4fcbf18cc0d8f38ee3a62abe2fa6991
Alexey Andreev <alexey.andreev@lykke.com>
2019-10-18 11:42:49 +0300

    LT-1672: temporarily remove apikey

--- src/Chest.Client/Compatibility/MetadataModel.cs
+++ src/Chest.Client/Compatibility/MetadataModel.cs
@@ -8,21 +8,11 @@ using System.Collections.Generic;
 
 namespace Chest.Client.AutorestClient.Models 
 {
-    using System.ComponentModel.DataAnnotations;
-
     /// <summary>
     /// Represents the data model
     /// </summary>
     public class MetadataModel
     {
-//        /// <summary>
-//        /// Gets or sets data against the given key
-//        /// </summary>
-//        [Required]
-//        [MinLength(1, ErrorMessage = "Provide a value for key data")]
-//        public string Data { get; set; }
-//
-//        public string Keywords { get; set; }
         public MetadataModel()
         {
         }
