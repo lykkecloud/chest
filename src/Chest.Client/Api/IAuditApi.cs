f36ab93016d0c30a9025430fd9c60a3c26a0e356
Gennady Ponomarev <gennady.ponomarev@lykke-business.ch>
2020-09-03 12:17:47 +0500

    LT-2506: Add audit

---
+++ src/Chest.Client/Api/IAuditApi.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Chest.Client.Models;
+using Chest.Client.Models.Requests;
+using Chest.Client.Models.Responses.Chest.Client.Models.Responses;
+using Refit;
+
+namespace Chest.Client.Api
+{
+    public interface IAuditApi
+    {
+        /// <summary>
+        /// Get audit logs
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
+        [Get("/api/v2/audit")]
+        Task<PaginatedResponseContract<AuditContract>> GetAuditTrailAsync([Query] GetAuditLogsRequest request,
+            int? skip = null, int? take = null);
+    }
+}
